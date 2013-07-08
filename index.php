20029af9d73ea74f481388982f2f5157abe5414c
Adam Backstrom <adam@sixohthree.com>
2013-07-08 12:27:43 -0400

    Default to ip-only dump for curl

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
 
-switch( $_SERVER['QUERY_STRING'] ) {
-    case 'txt':
-    case 'simple': die( $_SERVER['REMOTE_ADDR'] );
-    case 'fail': header('HTTP/1.0 404 Not Found'); die('404 Not Found');
-}
-
-function render($tpl, $params = array()) {
-    extract($params, EXTR_SKIP);
-    include $tpl;
-}
+$method = $_SERVER['QUERY_STRING'];
 
 $params = array(
     'host' => $_SERVER['HTTP_HOST'],
@@ -17,5 +8,20 @@ $params = array(
     'remote_host' => gethostbyaddr($_SERVER['REMOTE_ADDR']),
     'user_agent' => $_SERVER['HTTP_USER_AGENT'],
 );
+
+if ($method == null && strpos($params['user_agent'], 'curl/') === 0) {
+    die($params['remote_ip'] . "\n");
+}
+
+switch($method) {
+    case 'txt':
+    case 'simple': die($params['remote_ip']);
+    case 'fail': header('HTTP/1.0 404 Not Found'); die('404 Not Found');
+}
+
+function render($tpl, $params = array()) {
+    extract($params, EXTR_SKIP);
+    include $tpl;
+}
 
 render(__DIR__ . '/tpl/index.phtml', $params);
