0ba4b651b7eeaa594a4c4bf42dfc698e003ea659
Adam Backstrom <adam@sixohthree.com>
2013-07-08 12:21:08 -0400

    Templateize the migrated template file

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 switch( $_SERVER['QUERY_STRING'] ) {
-	case 'txt':
-	case 'simple': die( $_SERVER['REMOTE_ADDR'] );
-	case 'fail': header('HTTP/1.0 404 Not Found'); die('404 Not Found');
+    case 'txt':
+    case 'simple': die( $_SERVER['REMOTE_ADDR'] );
+    case 'fail': header('HTTP/1.0 404 Not Found'); die('404 Not Found');
 }
 
 function render($tpl, $params = array()) {
@@ -11,4 +11,11 @@ function render($tpl, $params = array()) {
     include $tpl;
 }
 
-render(__DIR__ . '/tpl/index.phtml');
+$params = array(
+    'host' => $_SERVER['HTTP_HOST'],
+    'remote_ip' => $_SERVER["REMOTE_ADDR"],
+    'remote_host' => gethostbyaddr($_SERVER['REMOTE_ADDR']),
+    'user_agent' => $_SERVER['HTTP_USER_AGENT'],
+);
+
+render(__DIR__ . '/tpl/index.phtml', $params);
