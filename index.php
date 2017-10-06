5f458389d13f8554acdf5426ddd80e4ddb2b9e99
Annika Backstrom <annika@sixohthree.com>
2017-10-06 15:11:32 +0000

    Add "headers" query string arg to dump request headers

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ $params = array(
     'remote_ip' => $_SERVER["REMOTE_ADDR"],
     'remote_host' => gethostbyaddr($_SERVER['REMOTE_ADDR']),
     'user_agent' => $_SERVER['HTTP_USER_AGENT'],
+    'headers_only' => array_key_exists('headers', $_GET),
+    'headers' => get_request_headers($_SERVER),
 );
 
 if ($method == null && strpos($params['user_agent'], 'curl/') === 0) {
@@ -29,6 +31,34 @@ switch($method) {
 function render($tpl, $params = array()) {
     extract($params, EXTR_SKIP);
     include $tpl;
+}
+
+function get_request_headers($headers) {
+    $headers = array_filter($headers, function($value, $key) {
+        return substr($key, 0, 5) === 'HTTP_';
+    }, ARRAY_FILTER_USE_BOTH);
+
+    $map = array(
+        'dnt' => 'DNT',
+    );
+
+    $tmp = [];
+    foreach ($headers as $key => $value) {
+        $key = strtolower($key);
+        $key = substr($key, 5);
+
+        // does key have a manual override?
+        if (array_key_exists($key, $map)) {
+            $tmp[$map[$key]] = $value;
+            continue;
+        }
+
+        $key = str_replace("_", "-", $key);
+        $key = ucwords($key, "-");
+        $tmp[$key] = $value;
+    }
+
+    return $tmp;
 }
 
 render(__DIR__ . '/tpl/index.phtml', $params);
